feca1905d034709243c60afce33af4b98fe62481
Mordziaty <pnader00@gmail.com>
2023-12-24 09:29:54 +0100

    functional test check if you can add element to the list

--- functional_tests.py
+++ functional_tests.py
@@ -1,18 +1,42 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 import unittest
 
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
-        # self.browser.implicitly_wait(3)
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_app_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
+
+        # web title and header contain word "List"
         self.assertIn('List', self.browser.title)
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
+        self.assertIn('List', header_text)
+
+        # correct input of things to do
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'), 'Wpisz rzecz do zrobienia'
+        )
+        # enter "Kupić pawie pióra" in text field
+        inputbox.send_keys('Kupić pawie pióra')
+
+        # press enter key, website displays
+        # "1: Kupić pawie pióra" as to do list element
+        inputbox.send_keys(Keys.ENTER)
+        table = self.browser_find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertTrue(
+            any(row.text == '1: Kupić pawie pióra' for row in rows)
+        )
+
         self.fail('End of test')
 
 
