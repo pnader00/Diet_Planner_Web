8278b7d23433213e27cccd5cb925c076dff58a43
Mordziaty <pnader00@gmail.com>
2024-01-05 11:55:34 +0100

    add lists class to models

--- lists/models.py
+++ lists/models.py
@@ -3,6 +3,10 @@ from django.db import models
 # Create your models here.
 
 
+class List(models.Model):
+    pass
+
+
 class Item(models.Model):
     text = models.TextField(default='')
-    pass
+    list = models.ForeignKey(List, default=None, on_delete=models.CASCADE)
